103168c1a572d5db433da5df45fa138598ebdf98
Leonardo Tallone <leonardo.gabriel.tallone@gmail.com>
2024-01-31 17:01:57 -0300

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <View style={styles.container}>
       <View style={styles.leftContainer}>
-        <Text style={styles.title}>Explore</Text>
+        <Text style={styles.title}>Explore.</Text>
       </View>
       <View style={styles.rightContainer}>
         <Image style={styles.image}source={require('../../assets/nasa-logo.png')}/> 
